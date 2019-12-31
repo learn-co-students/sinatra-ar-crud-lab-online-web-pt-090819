31a60881306d0d7d624e79d2dc3b816298532772
Amgad Malik <amgadmalik@yahoo.com>
2019-12-31 15:32:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end 
+  
+  get '/articles/new' do
+    erb :new
   end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    
+    redirect to "/articles/#{@article.id}"
+  end 
+
+  post '/articles' do
+    
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{Article.last.id}"
+  end
+  
+  delete '/posts/:id/delete' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+  end
+  
 end
+
