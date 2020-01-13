52046d8db80f5b90f2ca59d47cb4bf6a8e1ca2d6
Alexander Bispo <alexbispo173@gmail.com>
2020-01-13 06:22:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do 
     erb :new
   end
-  
+ 
   post '/articles' do
     Article.create(params)
-    redirect '/articles'
+    redirect "/articles/#{Article.last.id}"
   end
   
   get '/articles' do 
@@ -35,9 +35,19 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_post = Article.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_post.update(new_params)
+
+    redirect "/articles/#{id}"
+  end
   
-  delete '/posts/:id/delete' do
-    @article = Article.find(params["id"])
+  delete '/articles/:id/delete' do
+    @article = Article.find(params[:id])
     @article.destroy
     erb :delete
   end
